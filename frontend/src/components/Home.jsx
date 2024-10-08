81d1348f3acc62d936e534c8ef03279946b757bb
Agustin Glaiel <agustinglaiel@gmail.com>
2024-10-08 10:16:32 -0300

    estilos

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -37,45 +37,49 @@ const Home = () => {
   const groupedSessions = groupSessionsByLocation(sessions);
 
   return (
-    <div className="sessions-container">
-      <h2>Upcoming Events</h2>
-      {groupedSessions.map((group, index) => (
-        <div key={index} className="location-group">
-          <div className="location-header">
-            <h3>{group.location}</h3>
-            <p>{group.country}</p>
+    <div className="page-container">
+      <h2 className="page-title">Upcoming Events</h2>
+      <div className="sessions-container">
+        {groupedSessions.map((group, index) => (
+          <div key={index} className="location-group">
+            <div className="location-header">
+              <h3>{group.location}</h3>
+              <p>{group.country}</p>
+            </div>
+            <div className="session-island">
+              <ul>
+                {group.sessions.map((session) => (
+                  <li key={session.id} className="session-item">
+                    <div className="session-details">
+                      <span className="session-date">
+                        {new Date(session.date_start).toLocaleDateString(
+                          "en-GB",
+                          {
+                            day: "numeric",
+                            month: "short",
+                          }
+                        )}
+                      </span>
+                      <span className="session-name">
+                        {session.session_name}
+                      </span>
+                      <span className="session-time">
+                        {new Date(session.date_start).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    </div>
+                    <button className="prediction-button">
+                      Completar pronóstico
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className="session-island">
-            <ul>
-              {group.sessions.map((session) => (
-                <li key={session.id} className="session-item">
-                  <div className="session-details">
-                    <span className="session-date">
-                      {new Date(session.date_start).toLocaleDateString(
-                        "en-GB",
-                        {
-                          day: "numeric",
-                          month: "short",
-                        }
-                      )}
-                    </span>
-                    <span className="session-name">{session.session_name}</span>
-                    <span className="session-time">
-                      {new Date(session.date_start).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
-                    </span>
-                  </div>
-                  <button className="prediction-button">
-                    Completar pronóstico
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
