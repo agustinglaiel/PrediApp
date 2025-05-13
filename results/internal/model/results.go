b6cd303803a859e4740d48d2fb0c69ae3d312602
agustinglaiel <agustinglaiel@gmail.com>
2025-05-12 22:45:19 -0300

    arreglos en results

--- results/internal/model/results.go
+++ results/internal/model/results.go
@@ -5,25 +5,28 @@ import "time"
 // Result representa los resultados de una sesión para un piloto
 type Result struct {
     ID             int    `gorm:"primaryKey" json:"id"`
-    SessionID      int       `json:"session_id"` // Foreign key to sessions
+    SessionID      int       `json:"session_id"` 
     Session        Session   `gorm:"foreignKey:SessionID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE;"`
-    DriverID       int       `json:"driver_id" gorm:"type:int"`  // Foreign key to drivers
+    DriverID       int       `json:"driver_id" gorm:"type:int"`
     Driver         Driver    `gorm:"foreignKey:DriverID;constraint:OnDelete:CASCADE;OnUpdate:CASCADE;"`
-    Position       *int       `json:"position"`   // Posición del piloto en la carrera
-    Status         string    `json:"status"`     // Estado del piloto en la carrera
-    FastestLapTime float64   `json:"fastest_lap_time"` // Duración de la vuelta rápida en segundos (con decimales)
+    Position       *int       `json:"position"`   
+    Status         string    `json:"status"` 
+    FastestLapTime float64   `json:"fastest_lap_time"`
     CreatedAt      time.Time `gorm:"autoCreateTime" json:"created_at"`
     UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
 
 // Definir el modelo Driver solo con los campos que necesitas para el preload
 type Driver struct {
-    ID          int    `json:"id"`
-    FirstName   string `json:"first_name"`
-    LastName    string `json:"last_name"`
-    FullName    string `json:"full_name"`
-    NameAcronym string `json:"name_acronym"`
-    TeamName    string `json:"team_name"`
+    ID            int    `gorm:"primaryKey" json:"id"`
+    BroadcastName string `json:"broadcast_name"`
+    CountryCode   string `json:"country_code"`
+    DriverNumber  int    `json:"driver_number"`
+    FirstName     string `json:"first_name"`
+    LastName      string `json:"last_name"`
+    FullName      string `json:"full_name"`
+    NameAcronym   string `json:"name_acronym"`
+    TeamName      string `json:"team_name"`
 }
 
 // Definir el modelo Session solo con los campos que necesitas para el preload
